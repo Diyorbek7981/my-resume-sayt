3d90c6e2ebd9d1cae0d390c1de1f6ede702c3f76
Diyorbek7981 <abdurahimov.diyorbek7981@gmail.com>
2024-09-08 16:42:38 +0500

    sytemm

--- config/settings.py
+++ config/settings.py
@@ -117,8 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
